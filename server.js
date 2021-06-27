d0b2f015515ef4a5b6e78e95111df9e878107208
Fariba DK <faribacodes@gmail.com>
2021-06-27 14:07:44 -0700

    CRUD update

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
-const express = require('express');
+const express = require('express')
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const shortid = require('shortid')
+const shortid = require('shortid');
 
-const app = express();
-app.use(bodyParser.json())
+const app = express()
+//middlewares
+app.use(express.json())
 
-mongoose.connect('mongodb://localhost/react-funwear-shop', {
+//initialize mongo-db
+//2 params for connect func: url, 3 15multiple keys 12-
+ mongoose.connect('mongodb://localhost/react-funwear-shop', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
 });
 
-//product model
+// //product model -> the model w 2 params: name of the collection in the db 2. the list of fields of the model
 const Product = mongoose.model(
   "products",
   new mongoose.Schema({ //bunch of columns
-    _id: {type:String, default: shortid.generate},
+    _id: {type:String, default: shortid.generate},//we set default value
     title: String,
     description: String,
     image: String,
@@ -27,21 +29,30 @@ const Product = mongoose.model(
 
 //GET - method
 app.get("/api/products", async(req, res)=>{
+  //find is a promise so use async/await
   const products = await Product.find({})
+  console.log(products)
   res.send(products)
 })
 
-//POST - method
+// //POST - method
 app.post("/api/products", async(req, res)=>{
   const newProduct = await new Product(req.body).save()
   res.send(newProduct)
 })
 
-//DELETE
+// //UPDATE - method
+// app.put("/api/products/:id", async(req, res) =>{
+//   const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, {new: true})
+//   res.json(updatedProduct)
+// })
+
+//DELETE  using :id as placeholder
 app.delete("/api/products/:id", async (req, res) =>{
   const deletedProduct = await Product.findByIdAndDelete(req.params.id)
   res.send(deletedProduct)
 })
+
 
 
 const PORT = process.env.PORT || 5000
